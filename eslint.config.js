5b26556e19cfadb3c067ad9a4a12136b8f2e752b
kimmsoll <kmgorae@gmail.com>
2024-12-06 23:08:50 +0900

    feat: 초기 셋팅

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'prettier': prettier, // prettier 추가
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error', // prettier의 포맷팅 에러를 lint에러 리포팅으로 볼 수 있게 함
+    },
+  },
+)
