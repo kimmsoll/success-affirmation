553960142c59be0158a9add095e921a02767890d
kimmsoll <kmgorae@gmail.com>
2024-12-13 15:59:51 +0900

    style: 로그인 화면 color 변경

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -6,10 +6,10 @@ const Login = () => {
   };
 
   return (
-    <div className='h-full bg-zinc-950 flex justify-center items-center'>
+    <div className='h-full bg-black-500 flex justify-center items-center'>
       <div className='flex flex-col gap-20'>
         <header className='text-center'>
-          <h1 className='text-2xl text-neutral-50 whitespace-pre-wrap'>{`매일 당신의 목소리로\n성공을 확언하세요.`}</h1>
+          <h1 className='text-2xl font-bold text-gray-100  whitespace-pre-wrap'>{`매일 당신의 목소리로\n성공을 확언하세요.`}</h1>
         </header>
         <button onClick={handleKakaoAuthorize} type='button' className='flex justify-center'>
           <img src='img/kakao_login_medium_narrow.png' width='auto' />
