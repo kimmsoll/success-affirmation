d023d43dcda6ee627d2c356db3cc777625bf1ef2
kimmsoll <kmgorae@gmail.com>
2024-12-15 02:03:52 +0900

    style: Header 및 전역 스타일링 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import Button from './Button';
+import Button from 'components/Button';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -19,16 +19,18 @@ const Header = () => {
     }
   };
 
-  const handleClickAdd = () => {};
+  const handleClickAdd = () => {
+    navigate('/affirmation/create');
+  };
 
   return (
-    <header className='flex justify-end px-5 py-5 shadow shadow-gray-900'>
+    <header className='z-10 sticky top-0 w-full flex justify-end px-5 py-5 bg-black-600 shadow shadow-gray-900'>
       <ul className='flex gap-2'>
         <li>
           <Button onClick={handleClickLogout} size='sm' text='로그아웃' />
         </li>
         <li>
-          <Button onClick={handleClickLogout} size='sm' text='확언등록' />
+          <Button onClick={handleClickAdd} size='sm' text='확언등록' />
         </li>
       </ul>
     </header>
