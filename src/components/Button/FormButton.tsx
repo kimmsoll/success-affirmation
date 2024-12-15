2df04d3a5e7c129bddf3e1f0f96ed87f0f64acab
kimmsoll <kmgorae@gmail.com>
2024-12-15 18:07:44 +0900

    feat: FormButton 생성

---
+++ src/components/Button/FormButton.tsx
@@ -0,0 +1,18 @@
+interface Props {
+  text: string;
+  disabled?: boolean;
+}
+
+const FormButton = ({ text, disabled = false }: Props) => {
+  return (
+    <button
+      type='submit'
+      disabled={disabled}
+      className='w-44 h-10 text-base bg-yellow-500 text-gray-900 font-semibold rounded-full hover:bg-yellow-600 hover:text-gray-800 disabled:bg-yellow-600 disabled:text-gray-800 disabled:cursor-not-allowed'
+    >
+      {text}
+    </button>
+  );
+};
+
+export default FormButton;
