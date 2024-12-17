31d7c7992b9feb792a4ff9d9e6165c51a7be7518
kimmsoll <kmgorae@gmail.com>
2024-12-17 16:15:10 +0900

    chore: 카카오 로그인 버튼img 리소스 변경

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -12,7 +12,7 @@ const Login = () => {
           <h1 className='text-2xl font-bold text-gray-100  whitespace-pre-wrap'>{`매일 당신의 목소리로\n성공을 확언하세요.`}</h1>
         </header>
         <button onClick={handleKakaoAuthorize} type='button' className='flex justify-center'>
-          <img src='img/kakao_login_medium_narrow.png' width='auto' />
+          <img src='img/kakao_login_large_narrow.png' width='200px' />
         </button>
       </div>
     </div>
