2f1e1e5118d8e6e21c6e7bb82519cd6e358fd474
kimmsoll <kmgorae@gmail.com>
2024-12-26 17:18:05 +0900

    feat: LoadingSpinner 컴포넌트 생성

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,5 @@
+const LoadingSpinner = () => (
+  <div className='mt-20 border-t-4 border-t-yellow-600 border-4 border-gray-800 rounded-full w-12 h-12 animate-spin'></div>
+);
+
+export default LoadingSpinner;
