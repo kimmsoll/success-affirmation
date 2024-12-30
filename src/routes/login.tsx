fddc998aea02464f0c33a217b8337be6ca2a7658
kimmsoll <kmgorae@gmail.com>
2024-12-30 17:32:27 +0900

    style: 로그인 화면 타이핑 효과 추가

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,5 @@
+import Typical from 'react-typical';
+
 const Login = () => {
   const handleKakaoAuthorize = () => {
     window.Kakao.Auth.authorize({
@@ -9,7 +11,10 @@ const Login = () => {
     <div className='h-full flex justify-center items-center'>
       <div className='flex flex-col gap-12'>
         <header className='text-center'>
-          <h1 className='text-2xl font-bold text-gray-100  whitespace-pre-wrap'>{`매일 당신의 목소리로\n성공을 확언하세요.`}</h1>
+          <h1 className='text-2xl font-bold text-gray-100  whitespace-pre-wrap'>
+            <Typical steps={['매일 당신의 목소리로', 2000]} loop={1} wrapper='p' />
+            <Typical steps={[2000, '성공을 확언하세요.', 2000]} loop={1} wrapper='p' />
+          </h1>
         </header>
         <button onClick={handleKakaoAuthorize} type='button' className='flex justify-center'>
           <img src='/img/kakao_login_large_narrow.png' width='200px' />
